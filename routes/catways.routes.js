3a4fc63381311578c612593c033afdd78334876b
Saxum-0 <165333677+Saxum-0@users.noreply.github.com>
2025-05-12 20:06:00 +0200

    Remove vite-static-copy from config

--- routes/catways.routes.js
+++ routes/catways.routes.js
@@ -13,6 +13,7 @@ router.patch('/:id', catwaysCtrl.updateCatway);         // PATCH /catways/:id
 router.delete('/:id', catwaysCtrl.deleteCatway);        // DELETE /catways/:id
 
 // ✅ Sous-routes RESTful pour les réservations liées à un catway
-router.use('/:id/reservations', reservationsRoutes);    // e.g. /catways/:id/reservations
+router.use('/catways/:id/reservations', reservationsRoutes);
+   // e.g. /catways/:id/reservations
 
 module.exports = router;
