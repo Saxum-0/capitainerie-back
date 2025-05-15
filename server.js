15fc10e3449633aa5f0dd7d1f7f8839d80720ba6
Saxum-0 <165333677+Saxum-0@users.noreply.github.com>
2025-05-15 10:26:01 +0200

    test auto

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  
+  console.log(`✅ Serveur lancé sur le port ${PORT}`);
 
   // Lancer les tests automatiquement
   if (process.env.NODE_ENV !== 'production') {
